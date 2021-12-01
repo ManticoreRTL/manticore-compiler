6e4dac307a3308a5a6a5501f2cc18d95eb3bd4cf
Sahand Kashani <sahand.kashani@gmail.com>
2021-12-01 15:14:43 +0100

    (MASM) Add test for RemoveAliases

--- code/manticore_isa/src/test/scala/manticore/assembly/levels/unconstrained/RemoveAliasesTester.scala
+++ code/manticore_isa/src/test/scala/manticore/assembly/levels/unconstrained/RemoveAliasesTester.scala
@@ -5,7 +5,7 @@ import manticore.UnitTest
 import manticore.compiler.AssemblyContext
 import manticore.assembly.parser.AssemblyParser
 
-class OrderInstructionsTester extends UnitTest {
+class RemoveAliasesTester extends UnitTest {
 
   import manticore.assembly.levels.unconstrained.UnconstrainedIR._
 
@@ -16,16 +16,23 @@ class OrderInstructionsTester extends UnitTest {
       @PROGRAM [name = "test"]
       .prog:
         .proc p0:
-          .input a 4
-          .input c 4
-          .input b 4
           .const c_0 4 0
-          .wire tmp1 4
+          .const c_1 4 0
+          .const c_2 4 2
+          .const c_3 4 2
+          .input a 4
+          .input b 4
+          .input c 4
           .output d 4
+          .wire tmp1 4
           .wire tmp2 4
-          ADD tmp2, tmp1, c;
-          ADD d, tmp2, c_0;
+          .wire tmp3 4
+          .wire tmp4 4
           ADD tmp1, a, b;
+          ADD tmp2, tmp1, c_0;
+          SUB tmp3, tmp2, c_2;
+          XOR tmp4, tmp3, c_3;
+          ADD d, tmp2, c_1;
       """
 
     val outputProgram = """
@@ -33,19 +40,26 @@ class OrderInstructionsTester extends UnitTest {
       .prog:
         .proc p0:
           .const c_0 4 0
+          .const c_1 4 0
+          .const c_2 4 2
+          .const c_3 4 2
           .input a 4
           .input b 4
           .input c 4
           .output d 4
           .wire tmp1 4
           .wire tmp2 4
+          .wire tmp3 4
+          .wire tmp4 4
           ADD tmp1, a, b;
-          ADD tmp2, tmp1, c;
-          ADD d, tmp2, c_0;
+          ADD tmp2, tmp1, c_0;
+          SUB tmp3, tmp1, c_2;
+          XOR tmp4, tmp3, c_2;
+          ADD d, tmp1, c_0;
       """
 
     val ctx = AssemblyContext()
-    val got = OrderInstructions(AssemblyParser(inputProgram, ctx), ctx)._1
+    val got = RemoveAliases(AssemblyParser(inputProgram, ctx), ctx)._1
     val expected = AssemblyParser(outputProgram, ctx)
 
     // // Debug
