70d81da9d5cdc2c13631283419ebcc5a1e3b38b2
Mahyar Emami <mayyxeng@gmail.com>
2022-05-18 19:45:10 +0200

    basic test infrastructure for yosys frontend

---
+++ src/test/scala/manticore/compiler/integration/yosys/unit/tiny/CounterTester.scala
@@ -0,0 +1,24 @@
+package manticore.compiler.integration.yosys.unit.tiny
+
+import manticore.compiler.integration.yosys.unit.YosysUnitTest
+import org.scalatest.fixture.UnitFixture
+import manticore.compiler.UnitFixtureTest
+import manticore.compiler.CompilationFailureException
+
+class CounterTester extends UnitFixtureTest {
+
+  behavior of "a simple counter"
+
+  it should "manticore should produce the same stdout as verilator" in { f =>
+
+      new YosysUnitTest {
+        val testIterations = 1000
+        // val topModule = "Counter"
+        val resourcePathString = "integration/yosys/tiny/counter.v"
+        val testDir = f.test_dir
+
+      }.run()
+
+
+  }
+}
