836ba858e28ce0777cc2e6aee95a0ca6fb69a63b
Mahyar Emami <mayyxeng@gmail.com>
2022-05-22 10:55:06 +0200

    flip flop testers also with async reset

--- src/test/scala/manticore/compiler/integration/yosys/unit/tiny/registers/FlipFlopTester.scala
+++ src/test/scala/manticore/compiler/integration/yosys/unit/tiny/registers/FlipFlopTester.scala
@@ -3,8 +3,9 @@ import manticore.compiler.UnitFixtureTest
 import manticore.compiler.integration.yosys.unit.TestCode
 import manticore.compiler.integration.yosys.unit.CodeText
 import manticore.compiler.integration.yosys.unit.YosysUnitTest
+import org.scalatest.CancelAfterFailure
 
-trait FlipFlopTester extends UnitFixtureTest {
+trait FlipFlopTester extends UnitFixtureTest with CancelAfterFailure {
 
   def name: String
   def generate(width: Int): TestCode
@@ -172,57 +173,73 @@ class SyncResetChipEnableDffTester extends FlipFlopTester {
 
 class AsyncResetDffTester extends FlipFlopTester {
     override def name: String = "$adff"
-// }
-// class AsyncResetWriteEnableDffTester extends FlipFlopTester {
 
-//     override def name = "$adffe"
+    override def generate(width: Int) = CodeText {
+        s"""|
+            |module SyncResetUnit(
+            |   input wire clk,
+            |   input wire arst,
+            |   input      [$width - 1 : 0] din,
+            |   output reg [$width - 1 : 0] dout,
+            |   input      [$width - 1 : 0] din_n,
+            |   output reg [$width - 1 : 0] dout_n
+            |);
+            |   always @(posedge clk or posedge arst) begin
+            |       if (arst == 1'b1) dout <= ${randGen.nextInt((1 << 31) - 1)};
+            |       else dout <= din;
+            |   end
+            |   always @(posedge clk or negedge arst) begin
+            |       if (arst == 1'b0) dout_n <= ${randGen.nextInt((1 << 31) - 1)};
+            |       else dout_n <= din_n;
+            |   end
+            |endmodule
+            |""".stripMargin
+    }
 
-//     override def generate(width: Int) = CodeText {
-//         s"""|
-//             |module SyncResetUnit(
-//             |   input wire clk,
-//             |   input wire srst,
-//             |   input wire en,
-//             |   input      [$width - 1 : 0] din_pp,
-//             |   output reg [$width - 1 : 0] dout_pp,
-//             |   input      [$width - 1 : 0] din_pn,
-//             |   output reg [$width - 1 : 0] dout_pn,
-//             |   input      [$width - 1 : 0] din_np,
-//             |   output reg [$width - 1 : 0] dout_np,
-//             |   input      [$width - 1 : 0] din_nn,
-//             |   output reg [$width - 1 : 0] dout_nn
-//             |);
-//             |   always @(posedge clk) begin
-//             |       if  (en == 1'b1) begin
-//             |           if (srst == 1'b1)
-//             |               dout_pp <= ${randGen.nextInt((1 << 31) - 1)};
-//             |           else  dout_pp <= din_pp;
-//             |       end
-//             |   end
-//             |   always @(posedge clk) begin
-//             |       if  (en == 1'b0) begin
-//             |           if (srst == 1'b1)
-//             |               dout_pn <= ${randGen.nextInt((1 << 31) - 1)};
-//             |           else  dout_pn <= din_pn;
-//             |       end
-//             |   end
-//             |   always @(posedge clk) begin
-//             |       if  (en == 1'b1) begin
-//             |           if (srst == 1'b0)
-//             |               dout_np <= ${randGen.nextInt((1 << 31) - 1)};
-//             |           else  dout_np <= din_np;
-//             |       end
-//             |   end
-//             |   always @(posedge clk) begin
-//             |       if  (en == 1'b0) begin
-//             |           if (srst == 1'b0)
-//             |               dout_nn <= ${randGen.nextInt((1 << 31) - 1)};
-//             |           else  dout_nn <= din_nn;
-//             |       end
-//             |   end
-//             |
-//             |endmodule
-//             |""".stripMargin
-//     }
 
-// }
+}
+class AsyncResetWriteEnableDffTester extends FlipFlopTester {
+
+    override def name = "$adffe"
+
+    override def generate(width: Int) = CodeText {
+        s"""|
+            |module SyncResetUnit(
+            |   input wire clk,
+            |   input wire arst,
+            |   input wire en,
+            |   input      [$width - 1 : 0] din_pp,
+            |   output reg [$width - 1 : 0] dout_pp,
+            |   input      [$width - 1 : 0] din_pn,
+            |   output reg [$width - 1 : 0] dout_pn,
+            |   input      [$width - 1 : 0] din_np,
+            |   output reg [$width - 1 : 0] dout_np,
+            |   input      [$width - 1 : 0] din_nn,
+            |   output reg [$width - 1 : 0] dout_nn
+            |);
+            |   always @(posedge clk or posedge arst) begin
+            |       if (arst == 1'b1)
+            |           dout_pp <= ${randGen.nextInt((1 << 31) - 1)};
+            |       else if (en == 1'b1) dout_pp <= din_pp;
+            |   end
+            |   always @(posedge clk or negedge arst) begin
+            |       if (arst == 1'b0)
+            |           dout_np <= ${randGen.nextInt((1 << 31) - 1)};
+            |       else if (en == 1'b1) dout_np <= din_np;
+            |   end
+            |   always @(posedge clk or posedge arst) begin
+            |       if (arst == 1'b1)
+            |           dout_pn <= ${randGen.nextInt((1 << 31) - 1)};
+            |       else if (en == 1'b0) dout_pn <= din_pn;
+            |   end
+            |   always @(posedge clk or negedge arst) begin
+            |       if (arst == 1'b0)
+            |           dout_nn <= ${randGen.nextInt((1 << 31) - 1)};
+            |       else if (en == 1'b0) dout_nn <= din_nn;
+            |   end
+            |endmodule
+            |""".stripMargin
+    }
+
+
+}
