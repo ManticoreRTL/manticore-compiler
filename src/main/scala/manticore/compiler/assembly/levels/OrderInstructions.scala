a5463a38beee1b346e2bb4a9554bd8dbec167832
Mahyar Emami <mayyxeng@gmail.com>
2022-07-01 16:37:15 +0200

    faster combinational loop check in order instruction

--- src/main/scala/manticore/compiler/assembly/levels/OrderInstructions.scala
+++ src/main/scala/manticore/compiler/assembly/levels/OrderInstructions.scala
@@ -41,67 +41,121 @@ trait CanOrderInstructions extends CanBuildDependenceGraph {
       )
       dependence_graph
     }
+
+    def findCycle(g: MutableGraph[Instruction, DiEdge]) = {
+
+      // a work around to make finding cycles faster: using g.findCycle
+      // takes too long if there is no cycle, so we perform our own dfs
+      // search to only call that function when there really is a cycle
+      sealed trait VisitState
+      case object InStack   extends VisitState
+      case object Visited   extends VisitState
+      case object Unvisited extends VisitState
+
+      val state = scala.collection.mutable.Map.empty[g.NodeT, VisitState] ++ g.nodes.map { _ -> Unvisited }
+
+      val stack = scala.collection.mutable.Stack.empty[g.NodeT]
+
+      def getCycle(n: g.NodeT): Iterable[g.NodeT] = {
+        stack.popWhile(_ != n)
+      }
+      def dfsCheck(n: g.NodeT): Iterable[g.NodeT] = {
+        def successorCycles(xs: Seq[g.NodeT]): Iterable[g.NodeT] = xs match {
+          case succ +: tail =>
+            if (state(succ) == InStack) {
+              getCycle(succ)
+            } else {
+              stack push succ
+              state(succ) = InStack
+              dfsCheck(succ)
+            }
+          case Nil => Nil
+        }
+
+        val cycle = successorCycles(n.diSuccessors.toSeq)
+        state(n) = Visited
+        stack.pop()
+        cycle
+      }
+
+      def find(nodes: Seq[g.NodeT]): Iterable[g.NodeT] = nodes match {
+        case n +: tail if (state(n) == Unvisited) =>
+          stack push n
+          state(n) = InStack
+          val cycle = dfsCheck(n)
+          if (cycle.isEmpty) {
+            find(tail)
+          } else {
+            cycle
+          }
+        case n +: tail =>
+          find(tail)
+        case Nil =>
+          Nil
+      }
+      find(g.nodes.toSeq)
+    }
     def reorder(
         instructionBlock: Iterable[Instruction]
     )(implicit ctx: AssemblyContext): Iterable[Instruction] = {
 
       val depGraph = dependenceGraph(instructionBlock)
 
-      (ctx.stats.recordRunTime("finding a cycle") { depGraph.findCycle }) match {
+      val cycle = ctx.stats.recordRunTime("finding a cycle") { findCycle(depGraph) }
+      if (cycle.nonEmpty) {
 
-        case Some(cycle) =>
-          ctx.logger.dumpArtifact("cycle.txt") {
-            cycle.nodes.map { _.toOuter.toString().take(50) }.mkString("->\n")
-          }
-          ctx.logger.error(
-            "Dependence graph is not acyclic, can not order instruction!"
-          )
+        ctx.logger.error("Found combinational loop!")
+        ctx.logger.dumpArtifact("cycle.txt") {
+          cycle.map { _.toOuter.toString().take(50) }.mkString("->\n")
+        }
 
-          ctx.logger.dumpArtifact(s"cyclic_dependence_graph_${transformId}.dot") {
-            GraphBuilder.toDotGraph(depGraph)
-          }
-          instructionBlock
-        case None => // ok
+        ctx.logger.error(
+          "Dependence graph is not acyclic, can not order instruction!"
+        )
+        ctx.logger.dumpArtifact(s"cyclic_dependence_graph_${transformId}.dot") {
+          GraphBuilder.toDotGraph(depGraph)
+        }
+        instructionBlock
 
-          // Alternative to performing DFS, we can use the topologicalSort visitor
-          // in the dependence_graph but that one is orders of magnitude slower than
-          // our simple DFS here, so don't use it :D
-          val schedule = scala.collection.mutable.Stack.empty[Instruction]
+      } else {
 
-          val to_schedule = depGraph.nodes.size
+        val schedule = scala.collection.mutable.Stack.empty[Instruction]
 
-          val scheduled_count = 0
+        val to_schedule = depGraph.nodes.size
 
-          val visited = scala.collection.mutable.Set.empty[depGraph.NodeT]
-          val unvisited = scala.collection.mutable.Queue
-            .empty[depGraph.NodeT] ++ depGraph.nodes
+        val scheduled_count = 0
 
-          def visitNode(n: depGraph.NodeT): Unit = {
-            if (!visited.contains(n)) {
+        val visited = scala.collection.mutable.Set.empty[depGraph.NodeT]
+        val unvisited = scala.collection.mutable.Queue
+          .empty[depGraph.NodeT] ++ depGraph.nodes
 
-              n.diSuccessors.foreach { succ =>
-                visitNode(succ)
-              }
-              visited += n
-              schedule push n.toOuter
+        def visitNode(n: depGraph.NodeT): Unit = {
+          if (!visited.contains(n)) {
+
+            n.diSuccessors.foreach { succ =>
+              visitNode(succ)
             }
+            visited += n
+            schedule push n.toOuter
           }
-          while (unvisited.nonEmpty) {
-            val n = unvisited.dequeue()
-            visitNode(n)
-          }
+        }
+        while (unvisited.nonEmpty) {
+          val n = unvisited.dequeue()
+          visitNode(n)
+        }
 
-          // IMPORTANT, popAll in scala 2.13.7 is broken! if you downgrade
-          // the scala version this will break!
+        // IMPORTANT, popAll in scala 2.13.7 is broken! if you downgrade
+        // the scala version this will break!
 
-          assert(
-            util.Properties.versionNumberString == "2.13.8",
-            "popAll requires care in scala earlier than 2.13.8"
-          )
+        assert(
+          util.Properties.versionNumberString == "2.13.8",
+          "popAll requires care in scala earlier than 2.13.8"
+        )
 
-          val orderedBlock = schedule.popAll()
-          orderedBlock
+        val orderedBlock = schedule.popAll()
+        orderedBlock
       }
+      // }
     }
     def reorder(process: DefProcess)(implicit ctx: AssemblyContext): DefProcess = {
       val orderedBody = reorder(process.body)
