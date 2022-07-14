3657ab1c09b233c8595b441bfe3e3f08e6cb896c
Sahand Kashani <sahand.kashani@gmail.com>
2022-07-14 17:55:33 +0200

    Add TVM-VTA deep learning accelerator benchmark

--- src/test/resources/integration/yosys/VHarness.cpp
+++ src/test/resources/integration/yosys/VHarness.cpp
@@ -14,17 +14,11 @@
 // Include model header, generated from Verilating the modules
 #include "VMain.h"
 
+#if VM_TRACE
 #include "verilated_vcd_c.h"
+#endif
 #include <chrono>
 #include <stdio.h>
-
-#ifdef VL_USER_FINISH
-// #define VL_USER_FINISH ///< Define this to override the vl_finish function
-void vl_finish(const char *filename, int linenum,
-               const char *hier) VL_MT_UNSAFE {
-  Verilated::gotFinish(true);
-}
-#endif
 // Legacy function required only so linking works on Cygwin and MSVC++
 double sc_time_stamp() { return 0; }
 
@@ -38,13 +32,15 @@ int main(int argc, char **argv, char **env) {
     printf("TIMEOUT not specified!\n");
     std::exit(-1);
   }
-  // Verilated::traceEverOn(true);
-  // VerilatedVcdC *tfp = new VerilatedVcdC;
-  // top->trace(tfp, 99); // Trace 99 levels of hierarchy
-  // tfp->open("trace.vcd");
+#if VM_TRACE
+  Verilated::traceEverOn(true);
+  VerilatedVcdC *tfp = new VerilatedVcdC;
+  top->trace(tfp, 99); // Trace 99 levels of hierarchy
+  tfp->open("trace.vcd");
+#endif
 
   unsigned int time_out = std::stoi(argv[1]) << 1;
-  // printf("Timeout cycles = %u\n", time_out >> 1);
+  printf("Timeout cycles = %u\n", time_out >> 1);
   int time = 0;
   top->clock = 0;
 
@@ -53,16 +49,21 @@ int main(int argc, char **argv, char **env) {
     time++;
     top->clock = !top->clock;
     top->eval();
-    // tfp->dump(time);
+#if VM_TRACE
+    tfp->dump(time);
+#endif
   }
   auto end = std::chrono::high_resolution_clock::now();
   auto duration =
       std::chrono::duration_cast<std::chrono::milliseconds>(end - start);
 
-  // printf("Finished after %d cycles in %.3f seconds\n", time >> 1,
-  // static_cast<float>(duration.count()) / 1000.); Final model cleanup
+  printf("Finished after %d cycles in %.3f seconds\n", time >> 1,
+         static_cast<float>(duration.count()) / 1000.);
+  // Final model cleanup
   top->final();
-  // tfp->close();
+#if VM_TRACE
+  tfp->close();
+#endif
 
   return 0;
 }
