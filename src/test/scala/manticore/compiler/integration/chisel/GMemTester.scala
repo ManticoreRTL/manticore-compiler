3c2e608396173aba67afbbaf1e6c346b4171c435
Keisuke Kamahori <keisuke1258@gmail.com>
2022-09-21 17:10:13 +0200

    Fix GMem tester to include back-to-back stores

--- src/test/scala/manticore/compiler/integration/chisel/GMemTester.scala
+++ src/test/scala/manticore/compiler/integration/chisel/GMemTester.scala
@@ -19,7 +19,9 @@ class GMemTester extends KernelTester {
       testSize: Int
   ): String = {
     val dir     = context.output_dir.get
-    val rnd     = XorShift16("rand", UInt16(12345))
+    val rnd1    = XorShift16("rand1", UInt16(123))
+    val rnd2    = XorShift16("rand2", UInt16(456))
+    val rnd3    = XorShift16("rand3", UInt16(789))
     val meminit = mkMemInit(Seq.fill(testSize) { UInt16(0) }, dir.toPath().resolve("mem.dat"))
 
     s"""
@@ -27,13 +29,23 @@ class GMemTester extends KernelTester {
        |   @LOC [x = 0, y = 0]
        |   .proc proc_0_0:
        |       ${meminit}
-       |       .mem mem_ptr 16 ${testSize}
+       |       .mem mem_ptr1 16 ${testSize}
+       |       ${meminit}
+       |       .mem mem_ptr2 16 ${testSize}
+       |       ${meminit}
+       |       .mem mem_ptr3 16 ${testSize}
        |
-       |       .wire st_val 16
-       |       .wire ld_val 16
+       |       .wire st_val1 16
+       |       .wire st_val2 16
+       |       .wire st_val3 16
+       |       .wire ld_val1 16
+       |       .wire ld_val2 16
+       |       .wire ld_val3 16
        |
        |
-       |       ${rnd.registers}
+       |       ${rnd1.registers}
+       |       ${rnd2.registers}
+       |       ${rnd3.registers}
        |
        |
        |       .const one 16 1
@@ -43,22 +55,36 @@ class GMemTester extends KernelTester {
        |
        |       ${mkReg("counter", Some(UInt16(0)))}
        |       ${mkReg("done", Some(UInt16(0)))}
-       |       ${mkReg("correct", Some(UInt16(1)))}
+       |       ${mkReg("correct1", Some(UInt16(1)))}
+       |       ${mkReg("correct2", Some(UInt16(1)))}
+       |       ${mkReg("correct3", Some(UInt16(1)))}
        |
        |
-       |       ${rnd.code}
+       |       ${rnd1.code}
+       |       ${rnd2.code}
+       |       ${rnd3.code}
        |
        |
-       |       ADD st_val, ${rnd.randNext}, zero;
-       |       (mem_ptr, 0) LST st_val, mem_ptr[counter_curr], one;
-       |       (mem_ptr, 1) LLD ld_val, mem_ptr[counter_curr];
+       |       ADD st_val1, ${rnd1.randNext}, zero;
+       |       ADD st_val2, ${rnd2.randNext}, zero;
+       |       ADD st_val3, ${rnd3.randNext}, zero;
+       |       (mem_ptr1, 0) LST st_val1, mem_ptr1[counter_curr], one;
+       |       (mem_ptr2, 0) LST st_val2, mem_ptr2[counter_curr], one;
+       |       (mem_ptr3, 0) LST st_val3, mem_ptr3[counter_curr], one;
+       |       (mem_ptr1, 1) LLD ld_val1, mem_ptr1[counter_curr];
+       |       (mem_ptr2, 1) LLD ld_val2, mem_ptr2[counter_curr];
+       |       (mem_ptr3, 1) LLD ld_val3, mem_ptr3[counter_curr];
        |
        |       ADD counter_next, counter_curr, one;
-       |       SEQ correct_next, ld_val, st_val;
+       |       SEQ correct1_next, ld_val1, st_val1;
+       |       SEQ correct2_next, ld_val2, st_val2;
+       |       SEQ correct3_next, ld_val3, st_val3;
        |
        |       SEQ done_next, counter_next, test_length;
        |
-       |       (0) ASSERT correct_curr;
+       |       (0) ASSERT correct1_curr;
+       |       (0) ASSERT correct2_curr;
+       |       (0) ASSERT correct3_curr;
        |       (1) FINISH done_curr;
        |
         """.stripMargin
