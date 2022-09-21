3075541e564277604ea71de7879a9d69b9d1c6ca
Keisuke Kamahori <keisuke1258@gmail.com>
2022-09-21 16:48:47 +0200

    Fix GMem tester
    Use KernelTester

--- src/test/scala/manticore/compiler/integration/chisel/GMemTester.scala
+++ src/test/scala/manticore/compiler/integration/chisel/GMemTester.scala
@@ -5,27 +5,21 @@ import chiseltest._
 import manticore.compiler.AssemblyContext
 import manticore.compiler.DefaultHardwareConfig
 import manticore.compiler.HasLoggerId
-import manticore.compiler.ManticorePasses
-import manticore.compiler.UnitFixtureTest
-import manticore.compiler.assembly.ManticoreAssemblyIR
-import manticore.compiler.assembly.annotations.DebugSymbol
 import manticore.compiler.assembly.levels.UInt16
 import manticore.compiler.assembly.levels.codegen.MachineCodeGenerator
 import manticore.compiler.assembly.utils.XorShift16
-import manticore.compiler.integration.chisel.util.ProcessorTester
-import manticore.compiler.integration.chisel.util.ProgramTester
-import org.scalatest.fixture.UnitFixture
+import manticore.compiler.integration.chisel.util.KernelTester
 
 import scala.annotation.tailrec
 
-class GMemTester extends UnitFixtureTest with ChiselScalatestTester with ProgramTester with ProcessorTester {
+class GMemTester extends KernelTester {
 
   def mkProgram(
       context: AssemblyContext,
       testSize: Int
   ): String = {
     val dir     = context.output_dir.get
-    val rnd     = XorShift16("rand1", UInt16(12345))
+    val rnd     = XorShift16("rand", UInt16(12345))
     val meminit = mkMemInit(Seq.fill(testSize) { UInt16(0) }, dir.toPath().resolve("mem.dat"))
 
     s"""
@@ -55,7 +49,7 @@ class GMemTester extends UnitFixtureTest with ChiselScalatestTester with Program
        |       ${rnd.code}
        |
        |
-       |       ADD st_val, ${rnd.randCurr}, zero;
+       |       ADD st_val, ${rnd.randNext}, zero;
        |       (mem_ptr, 0) LST st_val, mem_ptr[counter_curr], one;
        |       (mem_ptr, 1) LLD ld_val, mem_ptr[counter_curr];
        |
@@ -70,8 +64,7 @@ class GMemTester extends UnitFixtureTest with ChiselScalatestTester with Program
         """.stripMargin
   }
 
-  def createTest(
-      expected_vcycles: Int,
+  def createTestAndCompileAndRun(expected_vcycles: Int)(implicit
       fixture: FixtureParam
   ): Unit = {
     val context = AssemblyContext(
@@ -85,54 +78,14 @@ class GMemTester extends UnitFixtureTest with ChiselScalatestTester with Program
     )
 
     implicit val TestName = new HasLoggerId { val id = getTestName }
-    val text              = mkProgram(context, expected_vcycles)
-    fixture.dump("main.masm", text)
-    val program = compile(text, context)
-    // interpret the program to make sure it is correct before trying it
-    // out on Verilator
-    ManticorePasses.BackendInterpreter(true)(program)(context)
-
-    // now we need to generate machine code and give it to verilator
-    // we should also keep a list of all the messages we expect to observe
-
-    val assembled_program =
-      MachineCodeGenerator.assembleProgram(program)(context)
-
-    val sleep_cycles   = context.hw_config.maxLatency
-    val countdown_time = 2
-    val timeout =
-      (assembled_program.head.total + sleep_cycles) * (expected_vcycles) + 500
-    context.logger.info(s"time out is ${timeout} cycles")
-    // generate initialization files
-    MachineCodeGenerator.generateCode(assembled_program)(context)
-
-    context.logger.info("Starting hardware test")
-
-    test(
-      mkProcessor(context, 0, 0)
-    ).withAnnotations(Seq(VerilatorBackendAnnotation, WriteVcdAnnotation)) { dut =>
-      dut.clock.setTimeout(timeout)
-      context.logger.info("Programing the processor")
-      programProcessor(
-        dut,
-        assembled_program.head,
-        sleep_cycles,
-        countdown_time
-      )
-
-      context.logger.info("Waiting for finish")
-      while (!dut.io.periphery.exception.error.peek().litToBoolean) {
-        dut.clock.step()
-      }
-      // ensure no EXPECTs failed
-      assert(dut.io.periphery.exception.id.peek().litValue.toInt == 1)
-    }
+    val source            = mkProgram(context, expected_vcycles)
+    fixture.dump("main.masm", source)
+    compileAndRun(source, context)
   }
 
   behavior of "Global Memory"
-  it should "correctly handle global stores and loads" in { f =>
-    val randgen = new scala.util.Random(0)
-    createTest(expected_vcycles = 500, fixture = f)
+  it should "correctly handle global stores and loads" in { implicit f =>
+    createTestAndCompileAndRun(expected_vcycles = 500)
   }
 
 }
