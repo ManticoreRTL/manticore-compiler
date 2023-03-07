49db91c7f744fa54443d82b0083dfb70f8f74895
Sahand Kashani <sahand.kashani@gmail.com>
2023-03-07 10:30:08 +0100

    Escape and quote strings in DOT files generated by DeadCodeElimination

--- src/main/scala/manticore/compiler/assembly/levels/DeadCodeElimination.scala
+++ src/main/scala/manticore/compiler/assembly/levels/DeadCodeElimination.scala
@@ -120,6 +120,10 @@ trait DeadCodeElimination extends CanComputeNameDependence with CanCollectInputO
   )(implicit ctx: AssemblyContext): String = {
     import scalax.collection.io.dot._
     import scalax.collection.io.dot.implicits._
+
+    def escape(s: String): String = s.replaceAll("\"", "\\\\\"")
+    def quote(s: String): String  = s"\"${s}\""
+
     val dotRoot = DotRootGraph(
       directed = true,
       id = Some("List scheduling dependence graph")
@@ -152,7 +156,7 @@ trait DeadCodeElimination extends CanComputeNameDependence with CanCollectInputO
           dotRoot,
           DotNodeStmt(
             NodeId(nodeIndex(inode.toOuter)),
-            List(DotAttr("label", inode.toOuter.toString.trim.take(64)))
+            List(DotAttr("label", quote(escape(inode.toOuter.toString.trim.take(64)))))
           )
         )
       )
